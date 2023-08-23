fce9e1068c593d9338582ae8264253a09cd999e9
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-08-23 09:09:06 +0300

    update-2

--- src/index.js
+++
@@ -1,2 +0,0 @@
-import './js/header-height';
-import './js/modal';
--- src/js/header-height.js
+++
@@ -1,6 +0,0 @@
-// Динамически посчитать размер шапки
-const { height: headerHeight } = document
-  .querySelector('.header')
-  .getBoundingClientRect();
-
-document.body.style.paddingTop = `${headerHeight}px`;
